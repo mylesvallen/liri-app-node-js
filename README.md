a775a94e6db506fa098a03229ce80d64d4b6eae3
Myles Allen <mylesvallen@icloud.com>
2020-03-30 08:22:13 -0700

    readme

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. 
+# Welcome to Liri App (Node.js) 👋
+![Version](https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000)
+[![Documentation](https://img.shields.io/badge/documentation-yes-brightgreen.svg)](https://github.com/mylesvallen/liri-app-node-js#readme)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/mylesvallen/liri-app-node-js/graphs/commit-activity)
+[![License: ISC](https://img.shields.io/github/license/mylesvallen/Liri App (Node.js))](https://github.com/mylesvallen/liri-app-node-js/blob/master/LICENSE)
+[![Twitter: myleswebdev](https://img.shields.io/twitter/follow/myleswebdev.svg?style=social)](https://twitter.com/myleswebdev)
 
-LIRI will be a command line node app that takes in parameters and gives you back data.
+> LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+### 🏠 [Homepage](https://github.com/mylesvallen/liri-app-node-js#readme)
+
+## Author
+
+👤 **Myles V Allen**
+
+* Website: mylesallem.com
+* Twitter: [@myleswebdev](https://twitter.com/myleswebdev)
+* Github: [@mylesvallen](https://github.com/mylesvallen)
+* LinkedIn: [@linkedin.com\/in\/mylesallen](https://linkedin.com/in/linkedin.com\/in\/mylesallen)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/mylesvallen/liri-app-node-js/issues). You can also take a look at the [contributing guide](https://github.com/mylesvallen/liri-app-node-js/blob/master/CONTRIBUTING.md).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+
+## 📝 License
+
+Copyright © 2020 [Myles V Allen](https://github.com/mylesvallen).
+
+This project is [ISC](https://github.com/mylesvallen/liri-app-node-js/blob/master/LICENSE) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- liri.js
+++ liri.js
@@ -1,119 +1,116 @@
-//chalk is a npm package that was included to make the interface more attractive, fun and easy to read. 
+//chalk is a npm package that was included to make the interface more attractive, fun and easy to read.
 const chalk = require('chalk');
 const log = console.log;
 var align = require('align-text');
 var center = require('center-align');
 
-// on start function for node liri.js 
+// on start function for node liri.js
 function start() {
-    log("                                                                " +  
-        chalk.bgMagenta.bold("HELLO WELCOME TO MYLES' LIRI BOT") + 
-        "\n \n" + 
-        "                                                                    " + 
-        chalk.bgMagenta.bold("MY WISH IS YOUR COMMAND!\n") + "\n" +
-         "                                                                " + 
-        chalk.whiteBright("CHOOSE A COMMAND\n") + "\n" +
-         "                                                                " + 
-        chalk.cyan("1. my-tweets\n") + "\n" +
-         "                                                                " +
-        chalk.cyan("2. spotify-this-song\n") + "\n" +
-         "                                                                " +
-        chalk.cyan("3. movie-this\n") + "\n" +
-         "                                                                " +
-        chalk.cyan("4. do-what-it-says\n")
-        );
-
-           
-};
+	log(
+		'                                                                ' +
+			chalk.bgMagenta.bold("HELLO WELCOME TO MYLES' LIRI BOT") +
+			'\n \n' +
+			'                                                                    ' +
+			chalk.bgMagenta.bold('MY WISH IS YOUR COMMAND!\n') +
+			'\n' +
+			'                                                                ' +
+			chalk.whiteBright('CHOOSE A COMMAND\n') +
+			'\n' +
+			'                                                                ' +
+			chalk.cyan('1. my-tweets\n') +
+			'\n' +
+			'                                                                ' +
+			chalk.cyan('2. spotify-this-song\n') +
+			'\n' +
+			'                                                                ' +
+			chalk.cyan('3. movie-this\n') +
+			'\n' +
+			'                                                                ' +
+			chalk.cyan('4. do-what-it-says\n')
+	);
+}
 
 //'twitter' package assigned to the var myTweets for access to twitter API
 var Twitter = require('twitter');
 
-// //Node-Spotify-Api assigned to variable to pull data from Spotify's API 
+// //Node-Spotify-Api assigned to variable to pull data from Spotify's API
 var Spotify = require('node-spotify-api');
 
 //keys.js is a module with private access to Twitter & Spoitfy API's
 var keys = require('./keys.js');
 
-//request is used to grab data from the [OMDB API] 
-var request = require("request");
+//request is used to grab data from the [OMDB API]
+var request = require('request');
 
 //Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 var fs = require('fs');
 
-
-
 //one of the 4 commands that can be used
 // var doWhatItSays = doWhatItSays();
 
-
-//action for switch cases 
+//action for switch cases
 var action = process.argv[2];
 var value = process.argv[3];
 
 switch (action) {
-    case "my-tweets":
-        myTweets();
-        break;
+	case 'my-tweets':
+		myTweets();
+		break;
 
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
+	case 'spotify-this-song':
+		spotifyThisSong();
+		break;
 
-    case "movie-this":
-        movieThis();
-        break;
+	case 'movie-this':
+		movieThis();
+		break;
 
-    case "dowhatitsays":
-        doWhatItSays();
-        break;
+	case 'dowhatitsays':
+		doWhatItSays();
+		break;
 
-    default: 
-        start();
-};
-
-
-
-
-
+	default:
+		start();
+}
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<TWITTER CODE BELOW!!!!!>>>>>>>>>>>>>>>>>>>>>>>>>
 //==============================================================================
 
-
 function myTweets() {
+	var client = new Twitter({
+		consumer_key: keys.twitterKeys.consumer_key,
+		consumer_secret: keys.twitterKeys.consumer_secret,
+		access_token_key: keys.twitterKeys.access_token_key,
+		access_token_secret: keys.twitterKeys.access_token_secret
+	});
 
-    var client = new Twitter({
-        consumer_key: keys.twitterKeys.consumer_key,
-        consumer_secret: keys.twitterKeys.consumer_secret,
-        access_token_key: keys.twitterKeys.access_token_key,
-        access_token_secret: keys.twitterKeys.access_token_secret
-    });
+	var params = {
+		screen_name: 'myles_code'
+	};
 
-    var params = {
-        screen_name: 'myles_code',
-    };
-
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
-            for (var i = 0; i < tweets.length; i++) {
-                log(chalk.bold.underline.bgMagentaBright("Follow me on twitter @Myles_Code"))
-                log(center(chalk.italic.cyanBright(tweets[i].text), 175));
-            };
-        };
-
-    });
-};
-
-
-
-
+	client.get('statuses/user_timeline', params, function(
+		error,
+		tweets,
+		response
+	) {
+		if (!error) {
+			for (var i = 0; i < tweets.length; i++) {
+				log(
+					chalk.bold.underline.bgMagentaBright(
+						'Follow me on twitter @Myles_Code'
+					)
+				);
+				log(center(chalk.italic.cyanBright(tweets[i].text), 175));
+			}
+		}
+	});
+}
 
 //----------------------------------------------------------------------------
 //SPOTIFY SPOTIFY SPOTIFY SPOTIFY SPOTIFY SPOTIFY SPOTIFY SPOTIFY SPOTIFY
 // ****************  SPOTIFY CODE BELOW **********************************
 //----------------------------------------------------------------------------
-//   This will show the following information 
+//   This will show the following information
 //   about the song in your terminal/bash window
 //   * Artist(s)
 //   * The song's name
@@ -121,33 +118,36 @@ function myTweets() {
 //   * The album that the song is from
 
 function spotifyThisSong() {
+	var spotify = new Spotify({
+		id: keys.spotifyKeys.id,
+		secret: keys.spotifyKeys.secret
+	});
 
-    var spotify = new Spotify({
-        id: keys.spotifyKeys.id,
-        secret: keys.spotifyKeys.secret
-    });
+	spotify.search({ type: 'track', query: value }, function(err, data) {
+		if (err) {
+			return log('Error occurred: ' + err);
+		}
 
-    spotify.search({ type: 'track', query: value }, function(err, data) {
-        if (err) {
-            return log('Error occurred: ' + err);
-        }
-
-        log(chalk.bold.cyan("\nARTIST: ") + data.tracks.items[0].album.artists[0].name,
-            chalk.bold.magentaBright("\nSONG: "), data.tracks.items[0].name,
-            chalk.bold.cyan("\nPREVIEW LINK: "), chalk.red.underline(data.tracks.items[0].href),
-            chalk.bold.magentaBright("\nALBUM: "), data.tracks.items[0].album.name, "\n");
-    });
-};
+		log(
+			chalk.bold.cyan('\nARTIST: ') +
+				data.tracks.items[0].album.artists[0].name,
+			chalk.bold.magentaBright('\nSONG: '),
+			data.tracks.items[0].name,
+			chalk.bold.cyan('\nPREVIEW LINK: '),
+			chalk.red.underline(data.tracks.items[0].href),
+			chalk.bold.magentaBright('\nALBUM: '),
+			data.tracks.items[0].album.name,
+			'\n'
+		);
+	});
+}
 
 // //^^^^^^^__^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //          ___           __
 // |\  /|  |  | \  /  |  |__
-// | \/ |  |__|  \/   |  |__  
+// | \/ |  |__|  \/   |  |__
 //%%%%%%%%%%%%%%%%%%%%%%%%% OMDB CODE BELOW %%%%%%%%%%%%%%%%%%%%%%%%%
 //==============================================================================
-
-
-
 
 // * Title of the movie.
 // * Year the movie came out.
@@ -160,72 +160,68 @@ function spotifyThisSong() {
 // Then run a request to the OMDB API with the movie specified
 
 function movieThis() {
-    var nodeArgs = process.argv;
-    var movieName = "";
+	var nodeArgs = process.argv;
+	var movieName = '';
 
-    for (var i = 3; i < nodeArgs.length; i++) {
+	for (var i = 3; i < nodeArgs.length; i++) {
+		if (i > 3 && i < nodeArgs.length) {
+			movieName = movieName + '+' + nodeArgs[i];
+		} else {
+			movieName += nodeArgs[i];
+		}
+	}
 
-        if (i > 3 && i < nodeArgs.length) {
+	var queryUrl =
+		'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=40e9cece';
 
-            movieName = movieName + "+" + nodeArgs[i];
+	// This line is just to help us debug against the actual URL.
+	console.log(queryUrl);
 
-        } else {
-
-            movieName += nodeArgs[i];
-
-        }
-    };
-
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
-
-    request(queryUrl, function(error, response, body) {
-
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            log(chalk.bold.cyan("\nTITLE: "), chalk.bold.whiteBright(JSON.parse(body).Title),
-                chalk.bold.magentaBright("\nRELEASED IN: ") + chalk.bold.whiteBright(JSON.parse(body).Year),
-                chalk.bold.cyan("\nIMDB RATING: "), chalk.bold.redBright(JSON.parse(body).Ratings[0].Value),
-                chalk.bold.magentaBright("\nROTTEN TOMATOES RATING: "),
-                chalk.bold.redBright(JSON.parse(body).Ratings[1].Value),
-                chalk.bold.cyan("\nCOUNTRY RELEASED IN: "), chalk.bold.whiteBright(JSON.parse(body).Country),
-                chalk.bold.magentaBright("\nLANGUAGE: "), chalk.bold.whiteBright(JSON.parse(body).Language),
-                chalk.bold.cyan("\nPLOT: "), '"' + chalk.bold.bgMagenta(JSON.parse(body).Plot) + '"',
-                chalk.bold.magentaBright("\nACTORS: "), chalk.bold.green(JSON.parse(body).Actors)
-            );
-        };
-    });
-};
-
+	request(queryUrl, function(error, response, body) {
+		// If the request is successful
+		if (!error && response.statusCode === 200) {
+			// Parse the body of the site and recover just the imdbRating
+			// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+			log(
+				chalk.bold.cyan('\nTITLE: '),
+				chalk.bold.whiteBright(JSON.parse(body).Title),
+				chalk.bold.magentaBright('\nRELEASED IN: ') +
+					chalk.bold.whiteBright(JSON.parse(body).Year),
+				chalk.bold.cyan('\nIMDB RATING: '),
+				chalk.bold.redBright(JSON.parse(body).Ratings[0].Value),
+				chalk.bold.magentaBright('\nROTTEN TOMATOES RATING: '),
+				chalk.bold.redBright(JSON.parse(body).Ratings[1].Value),
+				chalk.bold.cyan('\nCOUNTRY RELEASED IN: '),
+				chalk.bold.whiteBright(JSON.parse(body).Country),
+				chalk.bold.magentaBright('\nLANGUAGE: '),
+				chalk.bold.whiteBright(JSON.parse(body).Language),
+				chalk.bold.cyan('\nPLOT: '),
+				'"' + chalk.bold.bgMagenta(JSON.parse(body).Plot) + '"',
+				chalk.bold.magentaBright('\nACTORS: '),
+				chalk.bold.green(JSON.parse(body).Actors)
+			);
+		}
+	});
+}
 
 // //@@@@@@@@@@@@@@@@@@@@@@@@@@ #DoWhatItSays CODE BELOW @@@@@@@@@@@@@@@@@@@@@@@@@
 // //==============================================================================
 
-
 function doWhatItSays() {
+	fs.readFile('random.txt', 'utf8', function(err, data) {
+		if (err) {
+			return console.log(err);
+		}
 
-    fs.readFile("random.txt", "utf8", function(err, data) {
-        if (err) {
-            return console.log(err);
-        }
+		var output = data.split(',');
 
+		// Loop Through the newly created output array
+		for (var i = 0; i < output.length; i++) {
+			// Print each element (item) of the array/
+			console.log([i]);
+		}
+	});
+}
+// *It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
 
-        var output = data.split(",");
-
-        // Loop Through the newly created output array
-        for (var i = 0; i < output.length; i++) {
-
-        // Print each element (item) of the array/
-        console.log([i]);
-        }
-    });
-
-};
-    // *It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-    // *Feel free to change the text in that document to test out the feature for other commands.
+// *Feel free to change the text in that document to test out the feature for other commands.
